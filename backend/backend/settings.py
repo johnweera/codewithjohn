0d2365bbd760d334b45fabdd59cbc588f5887c82
johnweera <johnweera.c@gmail.com>
2025-12-11 02:04:55 +0700

    Setup backend and frontend structure

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = []
 
